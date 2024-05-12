30a46fdfeeec3f938f3a3306c19a148572f7af07
Yuhan <special861204@gmail.com>
2024-05-12 15:13:10 +0800

    add ElectricCost to views

---
+++ src/views/Yuhan/ElectricCost.vue
@@ -0,0 +1,15 @@
+<script>
+import Calculator from '../components/Yuhan/Calculator.vue'
+export default{
+    data(){
+        return{
+        }
+    }
+}
+</script>
+
+<template>
+</template>
+
+<style scoped lang="scss">
+</style>
