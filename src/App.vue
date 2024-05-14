bc8cff9592f281339b48a36f3041021f5fbb2285
Yuhan <special861204@gmail.com>
2024-05-14 22:29:02 +0800

    modify path

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,9 @@ import Header from './components/Header.vue'
 
 <template>
   <header>
-    <div class="header">
-      <Header />
-    </div>
-    <h1>This is 首頁.</h1>
-
-        <RouterLink to="/">分頁1</RouterLink>
-        <RouterLink to="/">分頁2</RouterLink>
-        <RouterLink to="/oilInformation">oilInformation</RouterLink>
+    <RouterLink to="/ElectricCost">ElectricCost</RouterLink>
+    <RouterLink to="/">分頁2</RouterLink>
+    <RouterLink to="/oilInformation">oilInformation</RouterLink>
   </header>
 
   <RouterView />
