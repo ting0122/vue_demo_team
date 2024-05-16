c684a4f56fe73f92970ac0f2fe95383090d6f048
ZueHub <a0909095516@gmail.com>
2024-05-16 10:02:36 +0800

    energy

--- src/views/Taoyuan.vue
+++ src/views/Taoyuan.vue
@@ -43,6 +43,18 @@ export default {
 </script>
 
 <template>
+  <div class="content">
+
+<div style="position: relative; width: 60%; height: 400px; padding-top: 100px;
+padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy"
+    style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGFCShWQmU&#x2F;tfoCfektmGSkB9Ly21MKyQ&#x2F;view?embed"
+    allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+</div>
 <div class="headline">桃園市瓦斯價格列表</div>
  <div v-if="pitys===null">資料載入中</div> <!--設置空值，如果資料還未取得，就在畫面顯示 "資料載入中"  -->
  <div class="list" v-else>
@@ -172,5 +184,15 @@ top:800px;
   z-index: 1;
 
 
+}
+.content {
+  width: 100dvw;
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
+  /* flex-direction: column; 交錯軸線位置與主軸線位置調換，因此內容物呈現直向排列*/
+  /* justify-content: center; */
+  align-items: center;
+  opacity: 0.9;
 }
 </style>
